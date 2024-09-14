273c5fca36752d529587e0d2522735e7d1ba4a7b
quangtrungcode <quangtrung24122002@gmail.com>
2024-09-14 22:19:09 +0700

    update

--- STM32PROJECT/Core/Src/main.c
+++ STM32PROJECT/Core/Src/main.c
@@ -63,7 +63,7 @@ void SystemClock_Config(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+// TODO EX1
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
